c7af1144b9e8df70ac2f4c555a881ca4e54b84a0
Michal Puškel <michal.puskel@gmail.com>
2018-01-26 19:51:43 +0100

    added FireWorks

---
+++ MonzaCircuit/Monza/Assets/FireWorksTriggerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWorksTriggerController : MonoBehaviour {
+
+    private GameObject fireWorks;
+
+	// Use this for initialization
+	void Start () {
+        fireWorks = GameObject.FindGameObjectWithTag("FireWorksMaster");
+        fireWorks.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ferrari") || other.CompareTag("RedBull"))
+        {
+            fireWorks.SetActive(true);
+        }        
+    }
+}
